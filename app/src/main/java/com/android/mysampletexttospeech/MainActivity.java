bf123bfe9a89b2dfeec6357225464170075046ac
MOHIT RAJ <raj716980@gmail.com>
2021-11-17 23:10:35 +0530

    Intial Commit

---
+++ app/src/main/java/com/android/mysampletexttospeech/MainActivity.java
@@ -0,0 +1,35 @@
+package com.android.mysampletexttospeech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+   private EditText editText;
+   private TextToSpeech textToSpeech;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        editText = findViewById(R.id.Your_Text_Here);
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                textToSpeech.setLanguage(Locale.US);
+                textToSpeech.setSpeechRate((float) 0.5);
+            }
+
+        });
+    }
+
+        public void TextToSpeechButton(View view){
+            textToSpeech.speak(editText.getText().toString(),TextToSpeech.QUEUE_FLUSH,null,null);
+        }
+    }
